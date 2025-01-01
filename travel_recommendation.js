ca269ca7a16b04f2a0b40cfa52be20c4df2f28a9
Noah Stewart <everythingcubbies5@gmail.com>
2025-01-01 12:29:40 -0500

    Fixed styling on about us, added basic js variables

---
+++ travel_recommendation.js
@@ -0,0 +1,9 @@
+const contactName = document.getElementById("custName");
+const contactEmail = document.getElementById("custEmail");
+const contactMessage = document.getElementById("custMessage");
+const btnContactSubmit = document.getElementById("submitForm");
+
+const searchWords = document.getElementById("searchBar");
+const btnSearch = document.getElementById("btnSearch");
+const btnClear = document.getElementById("btnReset");
+
