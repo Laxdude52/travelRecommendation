91087fbe29abe26acd27254942dbb04414782ae5
Noah Stewart <everythingcubbies5@gmail.com>
2025-01-01 14:50:46 -0500

    More js progres

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -7,3 +7,19 @@ const searchWords = document.getElementById("searchBar");
 const btnSearch = document.getElementById("btnSearch");
 const btnClear = document.getElementById("btnReset");
 
+const catagories = ["beach", "temple", "countr"]
+
+btnSearch.addEventListener("click", () => {
+    let words = searchWords.value;
+    words = words.toLowerCase();
+    catagories.forEach((e) => {
+       if(words.includes(e)) {
+            words = e;
+       } 
+    });
+    showResults(words);
+});
+
+const showResults = (category) => {
+
+}
